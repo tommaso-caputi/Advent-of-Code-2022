66491384df643c1c715dd9c352be5d16c3a59b80
tommaso caputi <tommasocaputi85@gmail.com>
2022-12-11 21:44:58 +0100

    complete first part of day 6

--- 6/1.py
+++ 6/1.py
@@ -1,4 +1,8 @@
 with open("6.txt","r") as f:
-    list = f.read()
-
-print(list)
+    signal = f.read()
+for i in range(len(signal)-4):
+    temp = signal[i:i+4]
+    if len(temp)==len(set(temp)):
+        res=i+4
+        break
+print(res)
